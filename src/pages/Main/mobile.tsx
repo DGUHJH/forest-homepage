b1e53caf032cdb330f71993dd899fbf501097715
DGUHJH <ghkdwogud852@gmail.com>
2022-01-10 17:46:27 +0900

    Update : Mobile Layout

--- src/pages/Main/mobile.tsx
+++ src/pages/Main/mobile.tsx
@@ -1,7 +1,48 @@
+import logo from 'assets/images/logo.png';
+import background from 'assets/images/mobile_background.png';
+import { useRef, useState } from 'react';
 import * as Styled from './styled';
 
 const mobile = () => {
-  return <Styled.Root>mobile</Styled.Root>;
+  const [open, setOpen] = useState(false);
+  const ref = [useRef<any>(null), useRef<any>(null), useRef<any>(null)];
+
+  const onClick = (id: number) => () => {
+    ref[id].current?.scrollIntoView();
+    setOpen(false);
+  };
+
+  return (
+    <Styled.Root>
+      <Styled.MHeaderBody>
+        <Styled.MLogoImg src={logo} />
+        <Styled.MMenuButton onClick={() => setOpen((prev) => !prev)} />
+      </Styled.MHeaderBody>
+      <Styled.MDrawer anchor="right" open={open} onClose={() => setOpen(false)}>
+        <Styled.MDrawerBody>
+          <Styled.MDrawerMenuContainer>
+            <Styled.MDrawerMenuWrapper onClick={onClick(0)}>
+              <Styled.MDrawerArrowDownButton />
+              <Styled.MDrawerMenuTypo>기업소개</Styled.MDrawerMenuTypo>
+            </Styled.MDrawerMenuWrapper>
+            <Styled.MDrawerMenuWrapper onClick={onClick(1)}>
+              <Styled.MDrawerArrowDownButton />
+              <Styled.MDrawerMenuTypo>핵심가치</Styled.MDrawerMenuTypo>
+            </Styled.MDrawerMenuWrapper>
+            <Styled.MDrawerMenuWrapper onClick={onClick(2)}>
+              <Styled.MDrawerArrowDownButton />
+              <Styled.MDrawerMenuTypo>TEAM</Styled.MDrawerMenuTypo>
+            </Styled.MDrawerMenuWrapper>
+          </Styled.MDrawerMenuContainer>
+          <Styled.MDrawerCloseButton onClick={() => setOpen(false)} />
+        </Styled.MDrawerBody>
+      </Styled.MDrawer>
+      <Styled.MBar ref={ref[0]} style={{ top: '140vw' }} />
+      <Styled.MBar ref={ref[1]} style={{ top: '362vw' }} />
+      <Styled.MBar ref={ref[2]} style={{ top: '992vw' }} />
+      <Styled.MBackgroundImg src={background} />
+    </Styled.Root>
+  );
 };
 
 export default mobile;
--- src/pages/Main/styled.ts
+++ src/pages/Main/styled.ts
@@ -1,4 +1,5 @@
-import { Container, Typography } from '@material-ui/core';
+import { Container, Drawer, Typography } from '@material-ui/core';
+import { ArrowDownward, Close, Menu } from '@material-ui/icons';
 import styled from 'styled-components';
 
 export const Root = styled.div`
@@ -6,6 +7,7 @@ export const Root = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
+  position: relative;
 `;
 
 export const HeaderBody = styled.div`
@@ -56,4 +58,90 @@ export const BackgroundContainer = styled.div`
 export const BackgroundImg = styled.img`
   width: 1920px;
   margin-top: 120px;
+`;
+
+export const MHeaderBody = styled.div`
+  width: 100vw;
+  height: 13.8vw;
+  background: #fff;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  position: fixed;
+  padding: 0 5vw;
+  box-sizing: border-box;
+`;
+
+export const MLogoImg = styled.img`
+  width: 20vw;
+`;
+
+export const MMenuButton = styled(Menu)`
+  &&& {
+    font-size: 8vw;
+    cursor: pointer;
+  }
+`;
+
+export const MDrawer = styled(Drawer)``;
+
+export const MDrawerBody = styled.div`
+  &&& {
+    width: 60vw;
+    height: 100vh;
+    background: #e2e2e2;
+    padding: 5vw;
+    box-sizing: border-box;
+    position: relative;
+  }
+`;
+
+export const MDrawerCloseButton = styled(Close)`
+  &&& {
+    font-size: 8vw;
+    cursor: pointer;
+    position: absolute;
+    top: 5vw;
+    right: 5vw;
+  }
+`;
+
+export const MDrawerMenuContainer = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  margin-top: 8vw;
+`;
+
+export const MDrawerMenuWrapper = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  border-bottom: 1px #939393 solid;
+  cursor: pointer;
+  margin-top: 6vw;
+`;
+
+export const MDrawerMenuTypo = styled(Typography)`
+  &&& {
+    font-size: 4vw;
+  }
+`;
+
+export const MDrawerArrowDownButton = styled(ArrowDownward)`
+  &&& {
+    font-size: 4vw;
+    color: #939393;
+  }
+`;
+
+export const MBackgroundImg = styled.img`
+  width: 100vw;
+  margin-top: 13.8vw;
+`;
+
+export const MBar = styled.div`
+  width: 100vw;
+  height: 1px;
+  position: absolute;
 `;
